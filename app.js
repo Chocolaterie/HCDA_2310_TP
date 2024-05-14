056c2e908d6089b5ed40abd9745adff585506e32
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2024-05-14 16:05:37 +0200

    Correction tp 3

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
+// Connecte à la bdd
+mongoose.connect("mongodb://127.0.0.1:27017/db_article");
+
+// Afficher un message quand connectioné avec succès
+mongoose.connection.once('open', () => {
+    console.log(`Connecté(e) à la base`);
+});
+
+// Afficher message erreur si pas connecté
+mongoose.connection.on('error', () => {
+    console.log(`Erreur de connection à la base`);
+})
+
+// Déclarer le modele Article
+// 1 : Le nom de module - 2 : Les attributs du model - 3: Le nom de table/collection 
+const Article = mongoose.model("Article", { id: Number, title : String, content : String, author : String }, "articles");
+
+// APP
 const app = express();
 
 // Autoriser envoyer json dans le body
@@ -15,19 +35,21 @@ let DB_ARTICLES = [
 // ----------------------------------------------------
 // * ROUTES
 // ----------------------------------------------------
-app.get('/articles', (request, response) => {
+app.get('/articles', async (request, response) => {
 
-    response.json(DB_ARTICLES);
+    // Récupérer les articles via mongo
+    const articles = await Article.find();
+
+    response.json(articles);
 });
 
-app.get('/article/:id', (request, response) => {
+app.get('/article/:id', async (request, response) => {
 
     // Forcer à mettre en entier pour la validité de la condition ===
     const id = Number.parseInt(request.params.id);
 
     // Pour trouver un article avec l'id
-    // predicate
-    const foundArticle = DB_ARTICLES.find((article) => article.id === id);
+    const foundArticle = await Article.findOne({ id : id});
 
     // Si je ne trouve pas
     if (!foundArticle){
@@ -38,22 +60,23 @@ app.get('/article/:id', (request, response) => {
     return response.json(foundArticle);
 });
 
-app.post('/save-article', (request, response) => {
+app.post('/save-article', async (request, response) => {
     // Récupérer l'article envoyé
     const articleJson = request.body;
 
-    console.log(articleJson);
-    
     // UPDATE
     // Essaye de trouver un article existant si dans l'article envoyé y'a un id
     if (articleJson.id){
         // On veut modifier
         // -- récuperer et modifier
-        let article = DB_ARTICLES.find((article) => article.id === articleJson.id);
+        let article = await Article.findOne({ id : articleJson.id });
 
         article.title = articleJson.title
         article.content = articleJson.content
         article.author = articleJson.author
+
+        // -- save
+        await article.save();
 
         // Return = Arreter le code et envoye l'article modifié
         return response.json(article);
@@ -62,29 +85,35 @@ app.post('/save-article', (request, response) => {
     // CREATE
     // Si y'a pas l'id c'est une création
     // -- generer un id
-    articleJson.id = DB_ARTICLES.length + 1;
+    // -- compter combien d'article en base
+    const count = await Article.find().count();
+
+    articleJson.id = count + 1;
+
     // -- ajouter dans le tableau
-    DB_ARTICLES.push(articleJson);
+    const article = new Article(articleJson);
+
+    await article.save();
 
     // Retourner le json l'article crée
     return response.json(articleJson);
 });
 
-app.delete('/article/:id', (request, response) => {
+app.delete('/article/:id', async (request, response) => {
 
     // PARSER EN ENTIER l'ID de la requête (dans l'url)
     const id = Number.parseInt(request.params.id);
 
     // Tester que l'element existe
-    const indexToDelete = DB_ARTICLES.findIndex((article) => article.id === id);
+    const articleToDelete = await Article.findOne({ id : id });
 
     // Une erreur si trouve pas l'article
-    if (indexToDelete == -1){
+    if (!articleToDelete){
         return response.json({ message : `Impossible de supprimer un article inexistant`});  
     }
 
     // - supprimer
-    DB_ARTICLES.splice(indexToDelete, 1);
+    await articleToDelete.deleteOne();
 
     return response.json({ message : `Article ${id} supprimé avec succès`});
 });
